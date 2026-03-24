a0f1add84f62ae7990c136bd17f1a1aa3bb7612d
Tanmay Jagtap <tanmayjagtap831@gmail.com>
2026-03-24 15:37:55 +0530

    REVERSE UPDATED

--- Arrays/Hard/Reverse.java
+++ Arrays/Hard/Reverse.java
@@ -1,6 +1,30 @@
 import java.util.*;
-public class Reverse{
+
+public class Reverse {
+
+    static int[] reverse(int arr[]) {
+        int n = arr.length;
+        int ans[] = new int[n];
+        int j = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            ans[j++] = arr[i];
+        }
+        return ans;
+    }
+
+    static void printarray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        
+        int arr[] = {1, 2, 3, 4, 5, 6};
+
+        int ans[] = reverse(arr);
+
+        printarray(ans);   // ✅ fixed
     }
 }
